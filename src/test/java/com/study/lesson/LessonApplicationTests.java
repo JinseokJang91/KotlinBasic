61bc2f5ced16a9babc855ad1ae5b6e258892b5df
Jinseok Jang <hya2089@gmail.com>
2025-04-20 19:18:19 +0900

    first commit

---
+++ src/test/java/com/study/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
